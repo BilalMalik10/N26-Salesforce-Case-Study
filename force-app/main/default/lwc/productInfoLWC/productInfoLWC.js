1c4c087b4028e904fd037aecffc60ce8544614dd
Bilal Malik <bilalmmalik10@gmail.com>
2021-06-14 01:32:38 +0500

    LWC Component to show Product Info

---
+++ force-app/main/default/lwc/productInfoLWC/productInfoLWC.js
@@ -0,0 +1,37 @@
+import { api, LightningElement, track, wire } from 'lwc';
+import getCaseInfo from '@salesforce/apex/ProductInfoController.getCaseInfo';
+
+export default class ProductInfoLWC extends LightningElement {
+    @track showSpinner = false;
+    @api recordId;
+    case = {};
+
+    //lifecycle hook fires when a component is inserted into the DOM
+    connectedCallback(){
+        //Enabling loading spinner
+        this.showSpinner = true;
+        getCaseInfo({caseId : this.recordId})
+        .then(result => {
+            this.case = result;
+            console.log('case --> ' + this.case);
+        })
+        .catch(error => {
+            this.case = undefined;
+            //show error in toast notification.
+            this.showToast('Error', error, 'error');
+            //Disabling the loading spinner
+            this.showSpinner = false;
+        });
+    }
+
+    showToast(toastTitle, toastMessage, toastType){
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: toastTitle,
+                message: toastMessage,
+                variant: toastType,
+            }),
+        );
+    }
+
+}
